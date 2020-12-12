efcbd7b268376665a808d1c5f43548b22c27b428
kefi123 <1625071980@qq.com>
2020-12-12 12:49:19 +0800

    测试代码

---
+++ src/main/java/xhj/love/tyj/test/Test.java
@@ -0,0 +1,13 @@
+package xhj.love.tyj.test;
+
+/**
+ * @Package xhj.love.tyj.test
+ * @Author 徐浩军
+ * @Date 2020/12/12 12:47
+ * @Version V1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
